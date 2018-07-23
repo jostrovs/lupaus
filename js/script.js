e2d00c09cd2209d1b9b7dc93f1c78ef6ae21f7b6
Jori Ostrovskij <jori.ostrovskij@granlund.fi>
2018-07-23 16:12:27 +0300

    Koodia

--- js/script.js
+++ js/script.js
@@ -1,29 +1,57 @@
+var bus = new Vue({
+    methods: {
+        on: function(event, callback){
+            this.$on(event, callback);
+        },
+        emit: function(event, payload){
+            this.$emit(event, payload);
+        }
+    }
+});
 
+let cnt=0;
 class Pelaaja {
     constructor(nimi){
+        this.id = cnt++;
         this.nimi = nimi;
         this.pisteet = 0;
+        
+        this.jaljessa = 0;
+        this.osumat=[];
     }
 }
 
+var ruutuPisteet = function(ruutu){
+    if(ruutu.osui){
+        if(ruutu.lupaus == 10) return 100;
+        return ruutu.lupaus + 10;
+    } else return 0;
+
+};
+
 class Ruutu {
-    constructor(pelaaja){
-        this.nimi = pelaaja.nimi;
+    constructor(pelaaja, onkoJakaja, no){
+        this.pelaaja = pelaaja;
         this.lupaus = 0;
         this.osui = false;
-        this.pelattu = 0;
+        this.pelattu = false;
+        this.jakaja=onkoJakaja; 
+        this.no = no;
     }
 }
 
 class Rivi { 
-    constructor(pelaajat, jakomäärä){
+    constructor(kierros, pelaajat, jakomäärä, jakajaNo){
         this.jakomäärä=jakomäärä;
+        this.kierros = kierros;
+        this.pelattu=false;
         this.ruudut = [];
+        let i=0;
         for(let pelaaja of pelaajat){
-            this.rivit.push(new Ruutu(pelaaja))
+            this.ruudut.push(new Ruutu(pelaaja, jakajaNo == i++, i))
         }
+    };
 
-    }
 }
 
 
@@ -33,16 +61,170 @@ $(document).ready(function () {
         data: {
             pelaajat: [new Pelaaja("Pelaaja #1"),
                        new Pelaaja("Pelaaja #2"),
-                       new Pelaaja("Pelaaja #2"),
-                       new Pelaaja("Pelaaja #2"),
-                       new Pelaaja("Pelaaja #2")],
+                       new Pelaaja("Pelaaja #3"),
+                       new Pelaaja("Pelaaja #4"),
+                       new Pelaaja("Pelaaja #5")],
+            
+            peliPelaajat: [],
+            rivit: [],
+            kierros: 1,
+
+            options: {
+                showTotal: true,
+                showJaljessa: true,
+            }
         },
         
         created: function () {
+            bus.on("player_down", this.player_down);
+            bus.on("player_up", this.player_up);
+            bus.on("player_clear", this.player_clear);
+
+        },
+
+        mounted(){
+            this.load();
         },
         computed: {
+            toJSON(){
+                let ret = {
+                    pelaajat: []
+                };
+                this.pelaajat.map(p=> { ret.pelaajat.push(p) });
+             
+                localStorage.saveObjs= ret;
+                return "d";
+            }
         },
         methods: {
+            player_clear(id){
+                let index=0;
+                for(;index<this.pelaajat.length;++index) if(this.pelaajat[index].id == id) break;
+                
+                this.pelaajat[index].nimi = "";
+            },
+            player_up(id){
+                let index=0;
+                for(;index<this.pelaajat.length;++index) if(this.pelaajat[index].id == id) break;
+                
+                if(index<1) return;
+
+                let pelaaja = this.pelaajat[index];
+                this.pelaajat.splice(index,1);
+
+                this.pelaajat.splice(index-1, 0, pelaaja);
+            },
+            player_down(id){
+                let index=0;
+                for(;index<this.pelaajat.length;++index) if(this.pelaajat[index].id == id) break;
+                
+                if(index>= this.pelaajat.length-1) return;
+
+                let pelaaja = this.pelaajat[index];
+                this.pelaajat.splice(index,1);
+
+                this.pelaajat.splice(index+1, 0, pelaaja);
+            },
+
+            luoUusi: function(){
+                // $("#navbar").hide();
+                // $("#etusivu").hide();
+                // $("#sivu1").show();
+
+                let kierros = 1;
+                this.peliPelaajat = [];
+                for(pelaaja of this.pelaajat){
+                    if(pelaaja.nimi == undefined) pelaaja.nimi = "";
+                    if(pelaaja.nimi.trim() != ""){
+                        this.peliPelaajat.push(new Pelaaja(pelaaja.nimi));
+                    }
+                }
+
+                let jakaja = this.peliPelaajat.length-1;
+
+                this.rivit = [];
+                for(let i=10;i>0;--i){
+                    this.rivit.push(new Rivi(kierros++, this.peliPelaajat, i, jakaja++));
+                    if(jakaja > this.peliPelaajat.length-1) jakaja=0;
+                }
+                for(let i=2;i<11;++i){
+                    this.rivit.push(new Rivi(kierros++, this.peliPelaajat, i, jakaja++));
+                    if(jakaja > this.peliPelaajat.length-1) jakaja=0;
+                }
+
+                this.kierros = 1;
+                this.save();
+                localStorage.save = false;
+                this.vikatabi();
+            },
+            laskePisteet: function(){
+                for(pelaaja of this.peliPelaajat){
+                    pelaaja.pisteet=0;
+                    pelaaja.osumat = [];
+                    for(rivi of this.rivit){
+                        for(ruutu of rivi.ruudut){
+                            if(ruutu.pelaaja.nimi == pelaaja.nimi){
+                                 pelaaja.pisteet += ruutuPisteet(ruutu);
+
+                                 if(ruutu.osui) pelaaja.osumat.push(1);
+                            }
+                        }
+                    }
+                }
+
+                // Haetaan maksimi
+                let max=0;
+                for(pelaaja of this.peliPelaajat){
+                    if(pelaaja.pisteet > max) max = pelaaja.pisteet;
+                }
+
+                // Merkataan, paljonko on jäljessä                
+                for(pelaaja of this.peliPelaajat){
+                    if(pelaaja.pisteet == max) pelaaja.jaljessa = max;
+                    else pelaaja.jaljessa = pelaaja.pisteet-max;
+                }
+            }, 
+            edellinen: function(){
+                if(this.kierros > 1) this.kierros--;
+            },
+            seuraava: function(){
+                this.save();
+
+                if(this.rivit[this.kierros-1].pelattu == false) return;
+                if(this.kierros < 19) this.kierros++;
+            },
+
+            load(){
+                if(localStorage.pelaajat) this.pelaajat = JSON.parse(localStorage.pelaajat);
+                if(localStorage.peliPelaajat) this.peliPelaajat = JSON.parse(localStorage.peliPelaajat);
+                if(localStorage.rivit) this.rivit = JSON.parse(localStorage.rivit);
+                if(localStorage.kierros) this.kierros = JSON.parse(localStorage.kierros);
+
+                if(localStorage.save){
+                     this.vikatabi();
+
+                     for(let i=0;i<20;++i) this.seuraava();
+
+                     this.options = JSON.parse(localStorage.options);
+                }
+            },
+
+            save(){
+                localStorage.pelaajat = JSON.stringify(this.pelaajat);
+                localStorage.peliPelaajat = JSON.stringify(this.peliPelaajat);
+                localStorage.rivit = JSON.stringify(this.rivit);
+                localStorage.kierros = JSON.stringify(this.kierros);
+                
+                localStorage.options = JSON.stringify(this.options);
+                
+                localStorage.save = true;
+            },
+
+            vikatabi(){
+                $('#navbar a:last').tab('show');
+            }
+
+            
         }
     });
 });
--- js/vue-components.js
+++ js/vue-components.js
@@ -1,8 +1,12 @@
 Vue.component('vue-pelaaja',{
     template: `
-        <div>
-            <input type="text" v-model="pelaaja.nimi">
-        </div>
+            <div style="margin-top: 20px;">
+                <input type="text" v-model="pelaaja.nimi">
+                <button class="btn btn-sm" @click="clear"><img src="clear.png" width=20></button>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <button class="btn btn-sm" @click="up"><img src="up.png" width=20></button>
+                <button class="btn btn-sm" @click="down"><img src="down.png" width=20></button>
+            </div>
     `,
     props: ['pelaaja'],
     data: function () {
@@ -11,48 +15,155 @@ Vue.component('vue-pelaaja',{
             initialPelaaja: this.pelaaja,
         }
     },
+    methods: {
+        up(){
+            bus.emit("player_up", this.pelaaja.id);
+        },
+        down(){
+            bus.emit("player_down", this.pelaaja.id);
+        },
+        clear(){
+            bus.emit("player_clear", this.pelaaja.id);
+        }
+    }
     
 });
 
-Vue.component('vue-rivi', {
-                template: `
-                    <div class="form-group">
-                        <label>{{initialRivi.aihe_nimi}}</label>
-                        <label class="radio-inline"><input @change="onInput()" required type="radio" :name="radioname" value="1">1</label>
-                        <label class="radio-inline"><input @change="onInput()" required type="radio" :name="radioname" value="2">2</label>
-                        <label class="radio-inline"><input @change="onInput()" required type="radio" :name="radioname" value="3">3</label>
-                        <label class="radio-inline"><input @change="onInput()" required type="radio" :name="radioname" value="4">4</label>
-                        <label class="radio-inline"><input @change="onInput()" required type="radio" :name="radioname" value="5">5</label>
-                        valittu: {{valittu}}
+Vue.component('vue-ruutu',{
+    template: `
+        <div style="width: 99%;">
+            
+            <div  :id="id" class="ruutu" v-bind:style="styleObject">    
+            
+                <div style="display: inline-block;">
+
+                    {{ruutu.pelaaja.nimi}} <div v-for="osuma in ruutu.pelaaja.osumat" style="display: inline-block; height: 6px; width: 6px; background-color: yellow; border: 1px solid red"> </div><br>
+                    <div v-if="ruutu.pelattu">
+                        <span v-if="ruutu.osui">{{pinnat(ruutu)}}</span>
+                        <span v-if="!ruutu.osui">##</span>
                     </div>
-                `,
-                props: ['rivi'],
-                data: function () {
-                    return {
-                        randomId: this._uid,
-                        initialRivi: this.rivi,
-                        valittu: 0,
-                        radioname: "opt" + this.rivi.aihe_no,
-                    }
-                },
-                methods: {
-                    onInput: function () {
-                        let val = $(`input[name=${this.radioname}]:checked`).val();
-                        this.rivi.arvosana = val;
-                        this.valittu = val;
-                    }
-                },
-                created: function(){
-                    let self=this;
-                    setTimeout(function(){
-                        let $radios = $(`input[name=${self.radioname}]`);
-                        let radios = $.makeArray($radios);
-                        for(let radio of radios){
-                            if(radio.value == self.initialRivi.arvosana){
-                                $(radio).prop("checked", true);
-                            }
-                        }
-                    }, 10);
-                }
+                    <div v-if="!ruutu.pelattu" style="font-size: 20px;">
+                        <input style="margin: 10px; margin-right: 30px;" type="checkbox" v-model="ruutu.osui">
+                        <template v-if="ruutu.osui">{{pinnat(ruutu)}}</template>
+                        <template v-if="!ruutu.osui">{{ruutu.lupaus}}</template>
+                        <button v-if="!ruutu.osui" class="btn btn-lg" @click="minus" style="margin-left: 30px;" >-</button>
+                        <button v-if="!ruutu.osui" class="btn btn-lg" @click="plus">+</button>
+                    </div>
+                </div>
+
+                <div v-if="opts.showTotal" style="font-size: 20px; margin-top: 20px; text-align: center; display: inline-block; float: right; background: yellow; color: black; border-radius: 4px; padding: 2px; border: 1px solid brown; min-width: 60px;">
+                    <span style="font-size: 10px; vertical-align: absmiddle;">Yht:</span>
+                    {{ruutu.pelaaja.pisteet}}
+                </div>
+                <div v-if="opts.showJaljessa && ruutu.pelaaja.jaljessa >= 0" style="font-size: 20px; margin-top: 20px; text-align: center; display: inline-block; float: right; background: greenyellow; color: black; border-radius: 4px; padding: 2px; border: 1px solid brown; min-width: 60px;">
+                    {{ruutu.pelaaja.jaljessa}}
+                </div>
+                <div v-if="opts.showJaljessa && ruutu.pelaaja.jaljessa < 0" style="font-size: 20px; margin-top: 20px; text-align: center; display: inline-block; float: right; background: pink; color: black; border-radius: 4px; padding: 2px; border: 1px solid brown; min-width: 60px;">
+                    {{ruutu.pelaaja.jaljessa}}
+                </div>
+            </div>
+            <div v-if="ruutu.jakaja" style="display: inline-block; width: 10px; background: white; margin-top: 15px; height: 50px; border-radius: 4px">&nbsp;</div>
+        </div>
+    `,
+    props: ['ruutu', 'rivi', 'options'],
+    data: function () {
+        return {
+            opts: this.options,
+            randomId: this._uid,
+            initialRuutu: this.ruutu,
+            id: "ruutu" + this.ruutu.no,
+        }
+    },
+    computed: {
+        styleObject: function(){
+            if(this.ruutu.jakaja){
+                return {
+                    // 'border-width': '10px'
+                };
+            }
+
+            return{
+                // 'margin-left': '8px'
+            }
+        }
+    },
+    methods: {
+        minus: function(){
+            if(this.ruutu.lupaus > 0) this.ruutu.lupaus--;
+        },
+        plus: function(){
+            if(this.ruutu.lupaus < this.rivi.jakomäärä) this.ruutu.lupaus++;
+        },
+        pinnat: function(ruutu){
+            return ruutuPisteet(ruutu);
+        }
+    }
+});
+
+
+Vue.component('vue-rivi',{
+    template: `
+        <div v-if="kierros==rivi.kierros">
+            <div>
+                <div style="display: inline-block; margin-top: 10px; font-size: 18px">
+                    Jaetaan: <span style="font-weight: bold; color: yellow; font-size: 22px;">{{rivi.jakomäärä}}</span> korttia
+                </div>            
+                <div style="display: inline-block; float: right;">
+                    Kierros pelattu
+                    <input style="margin: 15px;" @change="riviTsekattu()" type="checkbox" v-model="rivi.pelattu">
+                </div>            
+            </div>
+            <div>
+                <div v-for="ruutu of rivi.ruudut">
+                    <vue-ruutu :ruutu="ruutu" :rivi="rivi" :options="opts"></vue-ruutu>
+                </div>
+            </div>
+
+            <div style="background: red; border: 2px solid pink; color: white; font-size: 20px; padding: 12px; margin-top: 20px;" v-if="!summaKunnossa">Lupausten summa ei kelpaa!</div>
+            
+            <div style="background: orange; border: 2px solid brown; color: white; font-size: 20px; padding: 12px; margin-top: 20px;" v-if="eiSaaLuvata >= 0">Ei saa luvata: {{eiSaaLuvata}}</div>
+            <div style="background: green; border: 2px solid white; color: white; font-size: 20px; padding: 12px; margin-top: 20px;" v-if="eiSaaLuvata < 0">Saa luvata mitä vain</div>
+
+
+        </div>
+    `,
+    props: ['rivi', 'kierros', 'options'],
+    data: function () {
+        return {
+            opts: this.options,
+            randomId: this._uid,
+            initialRuutu: this.ruutu,
+        }
+    },
+    methods: {
+        riviTsekattu: function(){
+            for(ruutu of this.rivi.ruudut) ruutu.pelattu = this.rivi.pelattu;
+            this.$emit('rivi_tsek', event.target.value);
+        },
+
+    },
+    computed: {
+        
+        summaKunnossa: function(){
+            let sum = 0;
+            for(let ruutu of this.rivi.ruudut){
+                sum += ruutu.lupaus;
+            }
+            return sum != this.rivi.jakomäärä;
+        },
+
+        eiSaaLuvata: function(){
+            let sum = 0;
+            for(let ruutu of this.rivi.ruudut){
+                if(ruutu.jakaja) continue;
+                sum += ruutu.lupaus;
+            }
+
+            if(sum > this.rivi.jakomäärä) return -1;
+            return this.rivi.jakomäärä-sum;
+        }
+
+    }
+
 });
 
